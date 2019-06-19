edeadcb660dd00cf4f9c5acde8449a497e3055bc
Aidinski <aa9683@rit.edu>
2019-06-18 23:05:55 -0400

    image process and verification

--- TensorFlow/CourseraTensorFlowProject/loadTrainingData.py
+++ TensorFlow/CourseraTensorFlowProject/loadTrainingData.py
@@ -14,9 +14,15 @@ callbacks = myCallback()
 
 
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+
+training_images=training_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
 #You'll notice that all of the values in the number are between 0 and 255. If we are training a neural network, for various reasons it's easier if we treat all values as between 0 and 1, 
 #a process called 'normalizing'...and fortunately in Python it's easy to normalize a list like this without looping. You do it like this:
-training_images, test_images = (training_images / 255.0), (test_images / 255.0)
+test_images=test_images/255.0
+training_images=training_images / 255.0
 
 #to plot the data deriven from pictures uncomment the next 4 lines
 #import matplotlib.pyplot as plt
@@ -25,15 +31,21 @@ training_images, test_images = (training_images / 255.0), (test_images / 255.0)
 #print(training_images[1])
 
 
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)), 
-                                    tf.keras.layers.Dense(512, activation=tf.nn.relu), 
-                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
-
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
 model.compile(optimizer='adam',
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+model.summary()
 model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
